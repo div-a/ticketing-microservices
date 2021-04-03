aefa3dd16792a0054fb8f331cfafec92b3900640
DESKTOP-3G40DR1\Diva <diva.lonial@gmail.com>
2021-04-03 22:17:23 +0800

    generic build client hook

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({currentUser}) => {
     
@@ -6,18 +6,10 @@ const LandingPage = ({currentUser}) => {
     return <h1>Index </h1>;
 }
 
-LandingPage.getInitialProps = async ({ req }) => {
-    //windows obj only exists on browser
-    if (typeof window === 'undefined'){
-        const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-            headers: req.headers
-        });
-        return data;
-    }
-    else {
-        const { data } = await axios.get('/api/users/currentuser');
-        return data;
-    }
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser')
+    return data;
 };
 
 
