bd80ddc2dc4e1cc0004f2b3bd7332f60879c697d
DESKTOP-3G40DR1\Diva <diva.lonial@gmail.com>
2021-04-04 13:27:51 +0800

    dont require auth for current user middleware. add sign in and sign out pages. pass current user info through app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,33 @@
 // As we want css imported on every page, have to import it in this app file with wrapper.
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client';
+import Header from '../components/header'
 
-export default ({Component, pageProps}) => {
-    return <Component {...pageProps}></Component>
+const AppComponent =  ({ Component, pageProps, currentUser }) => {
+    console.log(Component)
+    console.log(pageProps)
+
+    return (
+        <div>
+            <Header currentUser={currentUser}></Header>
+            <Component {...pageProps}></Component>
+        </div>
+    )
 };
+
+AppComponent.getInitialProps = async appContext => {
+    const client = buildClient(appContext.ctx);
+    const {data} = await client.get('api/users/currentUser');
+
+    let pageProps = {};
+    if(appContext.Component.getInitialProps){
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+
+    return {
+        pageProps,
+        ...data
+    };
+}
+
+export default AppComponent;
